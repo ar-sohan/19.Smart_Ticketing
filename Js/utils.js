1473e36cabb04bf509a648bdae4f8c39e13ba2eb
ar-sohan <arsohan101@gmail.com>
2024-02-20 20:03:29 +0600

    total and grand total & coupon section completed

--- Js/utils.js
+++ Js/utils.js
@@ -2,9 +2,14 @@ var count = 0;
 
 function changeSelectedSeat(id) {
     count++;
-    console.log(count);
     document.getElementById('seatCount').innerText = count;
     document.getElementById('leftSeats').innerText = 40 - count;
+    document.getElementById('totalPrice').innerText = count * 550;
+    if (count > 3) {
+        document.getElementById('couponInput').removeAttribute('disabled');
+        document.getElementById('couponApply').removeAttribute('disabled');
+       
+    }
     const a1 = document.getElementById(id);
     a1.style.backgroundColor = "green";
     a1.style.color = "white";
@@ -25,9 +30,53 @@ function showSelectedSeats(id) {
     const selectedSeats = document.getElementById('selectedSeats');
     selectedSeats.appendChild(ul);
 }
+
+function couponApply() {
+    let couponInputField = document.getElementById('couponInput');
+    const couponInput = couponInputField.value;
+    if (couponInput === "NEW15") {
+        const discountedPrice = 22 * 15;
+        const discountContainer = document.getElementById('discountedPrice');
+        discountContainer.style.fontWeight = '700';
+        const discountTitle = document.createElement('h1');
+        discountTitle.innerText = "Discounted Price";
+        discountContainer.appendChild(discountTitle);
+
+        const discount = document.createElement('p');
+        discount.innerText = "BDT " + discountedPrice;
+        discountContainer.appendChild(discount);
+
+        document.getElementById('grandTotal').innerText = 2200 - discountedPrice;
+        document.getElementById('couponBox').classList.add("hidden");
+    }
+    else if (couponInput === "Couple 20") {
+        const discountedPrice = 22 * 20;
+        const discountContainer = document.getElementById('discountedPrice');
+        discountContainer.style.fontWeight = '700';
+        const discountTitle = document.createElement('h1');
+        discountTitle.innerText = "Discounted Price";
+        discountContainer.appendChild(discountTitle);
+
+        const discount = document.createElement('p');
+        discount.innerText = "BDT " + discountedPrice;
+        discountContainer.appendChild(discount);
+
+        document.getElementById('grandTotal').innerText = 2200 - discountedPrice;
+        document.getElementById('couponBox').classList.add("hidden");
+    }
+    else {
+        couponInputField.value = "";
+        alert("Wrong Coupon code");
+    }
+}
+
 function selectSet(id) {
-    const seat = document.getElementById(id);
-    seat.classList.add('disabled');
+    if (count === 4) {
+        alert("You are not allowed to select more!");
+        return;
+    }
     changeSelectedSeat(id);
     showSelectedSeats(id);
+    const seat = document.getElementById(id);
+    seat.setAttribute('onclick', '');
 }
